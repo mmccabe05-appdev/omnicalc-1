898eedc0eadac39b15dbcf43218e06bb9d9c4546
mmccabe05 <mmccabe@chicagobooth.edu>
2022-10-17 19:17:35 +0000

    random number pages all work now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,16 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
+  def calculate_random
+    @user_min = params.fetch("user_min").to_f
+    @user_max = params.fetch("user_max").to_f
+    @rand_between = rand(@user_min..@user_max)
+
+    render({ :template => "calculation_templates/random_results.html.erb" })
+  end
 end
